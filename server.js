9ba38884689b0d0268e1c8d7c23aebe1b9260ef3
moneshvenkul <mvvenkul@gmail.com>
2022-10-10 11:24:10 -0400

    created head.ejs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('pages/index');
     });
 
 app.listen(8080, function () {
