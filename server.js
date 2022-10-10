25e17a363aed32e28e65451988c47163557a7b3a
moneshvenkul <mvvenkul@gmail.com>
2022-10-10 12:19:18 -0400

    created product.ejs

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var {data} = require('./data.js');
 
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('pages/index');
+    res.render('pages/index',{
+        products: data,
+    });
     });
 
 app.listen(8080, function () {
